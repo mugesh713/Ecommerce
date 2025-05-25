852a5003c191160d778c13c640ceeae7184e8e50
mugesh713 <vmugeshvmugesh24@gmail.com>
2025-05-25 22:13:46 +0530

    f

--- store/admin.py
+++ store/admin.py
@@ -1,18 +1,23 @@
 from django.contrib import admin
-from .models import Product,Variation, ReviewRating
-# Register your models here.
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('product_name','price','stock','category','modified_date','is_available')
-    prepopulated_fields = {'slug': ('product_name',)}
-    fields = ('product_name', 'slug', 'description', 'price', 'image', 'stock', 'is_available', 'category', 'youtube_link')
+from .models import Product, Variation, ReviewRating, ProductGallery
+import admin_thumbnails
 
-class Variationadmin(admin.ModelAdmin):
-    list_display = ('product','variation_category','variation_value','is_active','created_date')
+@admin_thumbnails.thumbnail('image')
+class ProductGalleryInline(admin.TabularInline):
+    model = ProductGallery
+    extra = 1
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'price', 'stock', 'category', 'modified_date', 'is_available')
+    prepopulated_fields = {'slug': ('product_name',)}
+    inlines = [ProductGalleryInline]
+
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ('product', 'variation_category', 'variation_value', 'is_active')
     list_editable = ('is_active',)
     list_filter = ('product', 'variation_category', 'variation_value')
 
-
-
-admin.site.register(Product,ProductAdmin)
-admin.site.register(Variation,Variationadmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Variation, VariationAdmin)
 admin.site.register(ReviewRating)
+admin.site.register(ProductGallery)
